b51df0ef4408cf9567584bef8aa418e5de554a22
Shams Arefin Khan <robeeen@gmail.com>
2021-12-11 13:30:43 +0600

    Create Toggle Function.
    Create a state called redmore and useState to false, Putting a if else clause to show first 200 of Details description and by default we will kep it as false, so that a button see more can toggle the description. State value is now false.

--- react-tours-project/src/Tour.js
+++ react-tours-project/src/Tour.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 const Tour = ({id, image, info, price, name})=>{
+
+  const [readmore, setReadmore] = useState(false);
+
     return <article className="single-tour">
     <img src={image} alt={name} />
     <footer>
@@ -8,9 +12,15 @@ const Tour = ({id, image, info, price, name})=>{
         <h4>{name} | {id}</h4>
         <h4 className="tour-price">${price}</h4>
       </div>
-      <p>
-        {info}
-      </p>
+      <p>{/**Putting a if else clause to show first 200 of Details description
+       * and by default we will kep it as false, so that a button see more can toggle the description.
+       * state value is now false.
+       */}
+        {readmore ? info: `${info.substring(0, 200)}`}
+        <button onClick={(() => {setReadmore (!readmore)})}>
+          {readmore ? 'show less' : 'show more'}
+        </button>
+       </p>
       <button className='delete-btn'>not interested</button>
       
     </footer>
